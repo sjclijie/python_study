bdb6916e5e95b9c7e473ffe2f0463f04e2c9f7d4
jaylee <sjclijie@vip.qq.com>
2016-06-08 00:02:09 +0800

    update

--- 3.string.py
+++ 3.string.py
@@ -146,15 +146,116 @@ print unichr(4661)
 
 ########## 内建函数
 
+print "#######################################\n"
+
+#1. 将字符串第一个字符大写
+print string.capitalize("hello world")
+print "hello world".capitalize()
 
 
+#2. 填充
+print "|" + "hello world".center( 50 ) + "|"
 
 
+#3. string.count(str, start, end)   str出现现string中的次数
+print "hello world".count("o", 0, 8)
+
+#4. string.decode( encoding='UTF-8', errors='strict|ignore|replace')   解码
+
+#5. string.encode( encoding='UTF-8', errors='strict|ignore|replace' ) 编码 
+
+#6. string.endswith(str,  beg=0, end=len(string)  ) 检查string是否以str结束
+print "string".endswith("ing")
+
+#7. string.expandtabs( tabsize = 8 ) 把tan转为空格 
+print "Hello\tworld".expandtabs(4)
+
+#8. string.find( str, beg=0, end=len(string))  str是否包含在string中, 返回首次出现的index
+print "hello world".find("world")
+
+#9. string.index( str, beg=0, end=len(string) ) 和find一样，如果不存在会抛出异常
+try:
+    print "hello world".index("world111")
+except ValueError, e:
+    print e
+
+#10. string.isalnum() string不为空并且所有的字符都是字母或者数字 
+print "helloworld_".isalnum()
+print "hello1234".isalnum()
+
+#11. string.isalpha() string不为空并且所有的字符都是字母
+print "hello".isalpha()
+print "h1234".isalpha()
+
+#12. string.isdigit() string不为空并且所有的字符都是数字 
+print "1234xxx".isdigit()
+print "1234".isdigit()
+
+print "1234".isalnum()
+
+#13. string.islower() 所有字符都是小写
+print "abcdefgH".islower()
+print "abcdefg".islower()
+
+#14. string.isupper()  所有字符都是大写
+print "HELLO".isupper()
+
+#15. string.isspace() 只包含空格
+print "   ".isspace()
+
+#16. string.join(str)  以string为分隔符，将str合并为新字符串
+print "|".join("abc")
+
+#17. string.ljust(with) 左对齐+填充长度
+print "|" + "hello".ljust(50) + "|"
+print "|" + "hello".rjust(50) + "|"
+
+#18. lower upper 转换大小写
+print "hello".upper()
+print "HELLO".lower()
+
+#19. string.lstrip()  去除空格
+print "|"+"    he".lstrip() + "|"
+print "|"+"he    ".rstrip() + "|"
+
+#20. string.partition( str ) 从str出现的第一个位置起，把string切成一个三元祖
+print "helloworld".partition("llo")
+print "helloworld".partition("xxx")
+
+print "1aahelloworldaa1".rpartition("aa") #从右边开始
 
 
+#21. string.replace( str1, str2, num=string.count(str1) ) 把string中str1替换成str2
+print "hello world".replace( "l", "i", 2 )
+
+#22. string.rfind() 从字符串右边开始查找
+print "aaaaastringa".find("a")
+print "aaaaastringa".rfind("a")
+
+print "aaaaastringa".index("a")
+print "aaaaastringa".rindex("a")
 
 
+#23. string.split( str="", num=string.count(str) ) 以str为分隔符切片
+print "hello,world".split(",")
 
+#24. string.splitlines( num=string.count("\n") ) 按行分隔
+print "hello\nworld".splitlines()
+
+#25. string.startswith( str, beg=0, end=len(string) ) 检查字符串string是否以str开头
+print "helloworld".startswith("hello")
+
+#26. string.strip([obj]) 同时执行lstrip和rstrip
+print "|" + " helloworld ".strip() + "|"
+
+#27. string.swapcase() 翻转大小写
+print "HELLO world".swapcase()
+
+#28. string.title()
+print "hello world".title()
+
+#29. string.zfill(with) 返回长度为with的字符串，不够前面补0
+print "hello".zfill(10)
 
 
 
